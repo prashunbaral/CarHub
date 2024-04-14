ddc7e1d855f0ca8735a35b9b8d5c0f9191ee8897
prashunbaral <P.Baral2@wlv.ac.uk>
2024-04-14 12:28:28 +0545

    added vercel app

---
+++ utils/index.ts
@@ -0,0 +1,56 @@
+import { manufacturers } from "@/constants";
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps){
+    const {manufacturer, year, model, limit, fuel} = filters
+    const headers = {
+        'X-RapidAPI-Key': '3b8c90a9f8msh53f920746712629p182c70jsnad4edaa62ff6',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers,
+    });
+
+    const result = await response.json()
+    return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; 
+    const mileageFactor = 0.1; 
+    const ageFactor = 0.05;
+  
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage')
+
+    const {make, year, model} = car;
+    url.searchParams.append('customer', 'hrjavascript-mastery')
+    url.searchParams.append('make', make)
+    url.searchParams.append('modelFamily', model.split(' ')[0])
+    url.searchParams.append('zoomType', 'fullscreen')
+    url.searchParams.append('modelYear', `${year}`)
+    url.searchParams.append('angle', `${angle}`)
+
+    return `${url}`;
+
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(type, value)
+
+    const newPathname=`${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathname
+}
+
+
